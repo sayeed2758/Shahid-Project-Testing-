2defb68597bbed8566d4281c8433f1afb394a4dd
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-29 08:35:13 +0530

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+// EZEE VISION CHAMPUA - Firebase Web App configuration
+window.EV_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAAXE62v0nC_T0QqbUh5hJ9gDHoahMFOdc",
+  authDomain: "ezee-vision-champua.firebaseapp.com",
+  projectId: "ezee-vision-champua",
+  storageBucket: "ezee-vision-champua.firebasestorage.app",
+  messagingSenderId: "157634563496",
+  appId: "1:157634563496:web:d07e76481898d7153ae19d"
+};
