14d5f7b6dad8b23c0b877b5d7f04268cd133f4f5
sayeed2758 <sayeedbackupmail@gmail.com>
2026-08-29 16:47:09 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-const CACHE="ezee-student-v1";
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./assets/css/style.css"]))))
-self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request).catch(()=>caches.match("./index.html")))));
+const CACHE="ezee-student-v3";
+const CORE=["./","./index.html","./manifest.json","./assets/css/style.css","./assets/js/firebase-config.js","./assets/js/catalog.js","./assets/js/firebase.js","./assets/js/app.js","./assets/images/logo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
